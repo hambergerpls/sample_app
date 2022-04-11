5b64dff443f7483640889ea2f05634b39d20eaba
hambergerpls <danialhanif@outlook.com>
2022-04-11 14:44:09 +0800

    carrierwave fix 3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,11 +6,6 @@ if Rails.env.production?
             :aws_secret_access_key => ENV['S3_SECRET_KEY'],
             :region => ENV['S3_REGION']
         }
-
-        config.cache_dir = "#{Rails.root}/tmp/uploads"   
-
         config.fog_directory = ENV['S3_BUCKET']
-        config.cache_dir = "#{Rails.root}/tmp/uploads"
-        config.storage = :fog
     end
 end
