431ac21207d111aa21a2fe76158dba2571be2ca1
hambergerpls <danialhanif@outlook.com>
2022-04-11 14:30:49 +0800

    carrierwave fix 1

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,15 @@ if Rails.env.production?
         config.fog_credentials = {
             :provider => 'AWS',
             :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-            :aws_secret_access_key => ENV['S3_SECRET_KEY']
+            :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+            :region => ENV['S3_REGION']
         }
+
+        config.cache_dir = "#{Rails.root}/tmp/uploads"   
+
         config.fog_directory = ENV['S3_BUCKET']
-        config.fog_provider = 'fog/aws'
+        config.cache_dir = "#{Rails.root}/tmp/uploads"
+        config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET']}"
 
     end
 end
